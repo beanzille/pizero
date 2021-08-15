6b76efcba473130c72e3610e9c259e50161acd55
beanzille <49196759+beanzille@users.noreply.github.com>
2021-08-15 20:48:11 +0200

    Correct code for tut 1

---
+++ main.c
@@ -0,0 +1,16 @@
+//Correct code for Tut 1
+
+#include <stdio.h>
+int main(){
+        int a, b, sum;
+
+        printf("Enter a value for a: ");
+        scanf("%d", &a);
+
+        printf("Enter a value for b: ");
+        scanf("%d", &b);
+
+        sum = a + b;
+
+        printf("The sum of a+b is %d\n",sum);
+}
